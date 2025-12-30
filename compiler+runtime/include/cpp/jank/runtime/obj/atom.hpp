6ec5f17a53ffe5155cdbf776687e4471489f7965
cjbarre <cjbarre@gmail.com>
2025-12-29 23:59:30 -0800

    update ci

--- compiler+runtime/include/cpp/jank/runtime/obj/atom.hpp
+++ compiler+runtime/include/cpp/jank/runtime/obj/atom.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <atomic>
+
 #include <folly/Synchronized.h>
 
 #include <jank/runtime/object.hpp>
--- compiler+runtime/include/cpp/jank/runtime/var.hpp
+++ compiler+runtime/include/cpp/jank/runtime/var.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <atomic>
 #include <functional>
+#include <thread>
 
 #include <folly/Synchronized.h>
 
